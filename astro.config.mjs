3c71d091634b7006f2ba79a9c9cea2fb800567f2
Aaditya Chauhan <aaditya.chauhan21052007@gmail.com>
2026-04-03 20:00:11 +0530

    Initial Commit...

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
